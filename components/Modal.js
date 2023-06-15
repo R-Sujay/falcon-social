4b7156ca1253e290afbb940b9ccd5864187adf19
R-Sujay <sujay.rajesh08@gmail.com>
2023-06-15 10:44:02 +0530

    Done!

--- components/Modal.js
+++ components/Modal.js
@@ -42,7 +42,7 @@ function Modal() {
 
   return (
     <Transition.Root show={isOpen} as={Fragment}>
-      <Dialog as="div" className="fixed z-50 inset-0 pt-8" onClose={setIsOpen}>
+      <Dialog as="div" className="fixed z-[300] inset-0 pt-8" onClose={setIsOpen}>
         <div className="flex items-start justify-center min-h-[800px] sm:min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
           <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in duration-200" leaveFrom="opacity-100" leaveTo="opacity-0">
             <Dialog.Overlay className="fixed inset-0 bg-[#000] bg-opacity-40 transition-opacity" />
